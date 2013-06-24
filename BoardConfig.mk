365b6b6ba792f60d3abe5aac996e8b79799ce22b
ahman0 <kevtharp@gmail.com>
2013-06-23 20:56:31 -0700

    Added dependencies file and updating config and fixed type in recovery.fstab

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -1,5 +1,7 @@
 USE_CAMERA_STUB := true
 
+-include device/htc/msm7x30-common/BoardConfigCommon.mk
+
 # inherit from the proprietary version
 -include vendor/htc/vivow/BoardConfigVendor.mk
 
@@ -11,19 +13,20 @@ TARGET_CPU_ABI2 := armeabi
 TARGET_ARCH_VARIANT := armv7-a-neon
 ARCH_ARM_HAVE_TLS_REGISTER := true
 
-TARGET_KERNEL_SOURCE := kernel/htc/msm-8960
-TARGET_KERNEL_CONFIG := fighter_defconfig
+TARGET_KERNEL_SOURCE := kernel/htc/msm7x30
+TARGET_KERNEL_CONFIG := cyanogen_vivow_defconfig # fighter_defconfig
 TARGET_BOOTLOADER_BOARD_NAME := vivow
 
 BOARD_KERNEL_CMDLINE := no_console_suspend=1
 BOARD_KERNEL_BASE := 0x05000000
 BOARD_KERNEL_PAGESIZE := 2048
 
-# fix this up by examining /proc/mtd on a running device
+# sizes for ext4 partitions must be in decimal for the make_ext4fs tool
+TARGET_USERIMAGES_USE_EXT4 := true
 BOARD_BOOTIMAGE_PARTITION_SIZE := 0x00400000
 BOARD_RECOVERYIMAGE_PARTITION_SIZE := 0x0087f400
-BOARD_SYSTEMIMAGE_PARTITION_SIZE := 0x22dffe00
-BOARD_USERDATAIMAGE_PARTITION_SIZE := 0x496ffe00
+BOARD_SYSTEMIMAGE_PARTITION_SIZE := 585104896
+BOARD_USERDATAIMAGE_PARTITION_SIZE := 1232076288
 BOARD_FLASH_BLOCK_SIZE := 131072
 
 TARGET_PREBUILT_KERNEL := device/htc/vivow/kernel
